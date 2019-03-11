79e85368fac1289b4bc67fac2d08ca6e699d90a4
Stephen Mark <stephenjmark@gmail.com>
2019-03-11 15:07:12 -0400

    kube endpoint

--- config.js
+++ config.js
@@ -6,5 +6,7 @@ module.exports = {
     port: 5432,
     database: "newegg"
   },
-  url: "http://3.17.251.224"
+  url:
+    "http://aec1be695442c11e98b850e165ff3a6b-1728240150.us-east-1.elb.amazonaws.com"
+  // url: "http://3.17.251.224"
 };
